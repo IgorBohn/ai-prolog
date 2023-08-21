36a50f99c12a0ff117d4e2a786f689f77d2631df
Igor Bohn <igorfbohn@gmail.com>
2023-08-21 00:21:45 -0300

    feat: add ai prolog

---
+++ ia.pl
@@ -0,0 +1,140 @@
+
+/* CC */
+disciplina('CC', 1, 3, 35320, 'Introducao a Informatica', 45, 15, 0, 1).
+disciplina('CC', 1, 3, 30461, 'Interacao Humano Computador', 30, 30, 0, 1).
+disciplina('CC', 1, 3, 30701, 'Algoritmos e Estrutura de Dados I', 30, 60, 0, 1).
+disciplina('CC', 1, 3, 30737, 'Computador e Sociedade', 30, 0, 0, 1).
+disciplina('CC', 1, 7, 70899, 'Psicologia Aplicada', 30, 0, 0, 0).
+disciplina('CC', 1, 7, 72378, 'Metodologia da Pesquisa', 30, 0, 0, 0).
+
+disciplina('CC', 2, 3, 30762, 'Linguagem de Programacao I A', 45, 45, 30701, 0).
+disciplina('CC', 2, 3, 35324, 'Logica para a Computacao', 60, 0, 0, 1).
+disciplina('CC', 2, 3, 30702, 'Fisica Aplicada a Computacao', 30, 30, 0, 0).
+disciplina('CC', 2, 1, 10811, 'Geometria Analitica e Algebra Linear', 60, 0, 0, 0).
+disciplina('CC', 2, 8, 81285, 'Ingles Instrumental I', 30, 0, 0, 0).
+
+disciplina('CC', 3, 3, 30715, 'Seguranca e Auditoria de Sistemas', 30, 0, 0, 1).
+disciplina('CC', 3, 3, 35323, 'Linguagem de Programacao II', 30, 30, 30762, 1).
+disciplina('CC', 3, 3, 30774, 'Sistemas Digitais', 30, 30, 35324, 1).
+disciplina('CC', 3, 3, 30703, 'Algoritmos e Estruturas de Dados II', 30, 30, 30762, 0).
+disciplina('CC', 3, 1, 10229, 'Matematica Discreta', 60, 0, 0, 1).
+disciplina('CC', 3, 6, 66234, 'Legislacao em Informatica', 30, 0, 0, 0).
+
+disciplina('CC', 4, 3, 39705, 'Arquitetura de Computadores', 45, 15, 30774, 1).
+disciplina('CC', 4, 3, 35327, 'Banco de Dados I', 45, 15, '420 horas', 1).
+disciplina('CC', 4, 3, 30705, 'Algoritmos e Estruturas de Dados III', 30, 30, 30703, 0).
+disciplina('CC', 4, 3, 30704, 'Linguagem de Programacao III', 30, 30, 35323, 0).
+disciplina('CC', 4, 1, 10405, 'Calculo I', 60, 0, 0, 0).
+
+disciplina('CC', 5, 3, 39706, 'Arquiteturas Avancadas de Computadores', 60, 0, 39705, 0).
+disciplina('CC', 5, 3, 35328, 'Banco de Dados II', 45, 15, 35327, 1).
+disciplina('CC', 5, 3, 30709, 'Engenharia de Software I', 45, 15, '420 horas', 1).
+disciplina('CC', 5, 3, 38112, 'Sistemas Operacionais I', 45, 15, '420 horas', 1).
+disciplina('CC', 5, 1, 10406, 'Calculo II', 60, 0, 10405, 0).
+
+disciplina('CC', 6, 3, 30763, 'Fundamentos de Teoria da Computacao', 45, 15, '720 horas', 1).
+disciplina('CC', 6, 1, 15128, 'Calculo Numerico', 45, 15, 0, 0).
+disciplina('CC', 6, 3, 38113, 'Sistemas Operacionais II', 15, 15, 38112, 0).
+disciplina('CC', 6, 3, 30764, 'Redes de Computadores I', 45, 15, '720 horas', 1).
+disciplina('CC', 6, 3, 30706, 'Linguagem de Programacao IV', 15, 15, 30762, 0).
+disciplina('CC', 6, 3, 30710, 'Engenharia de Software II', 30, 30, 30709, 1).
+
+disciplina('CC', 7, 3, 30736, 'Estatistica Aplicada a Computacao', 45, 15, 0, 1).
+disciplina('CC', 7, 0, 0, 'Disciplina Eletiva', 60, 0, '990 horas', 1).
+disciplina('CC', 7, 3, 35346, 'Inteligencia Artificial', 45, 15, 35324, 1).
+disciplina('CC', 7, 3, 30765, 'Redes de Computadores II', 30, 30, 30764, 1).
+disciplina('CC', 7, 3, 30711, 'Engenharia de Software III', 15, 45, 30710, 1).
+
+disciplina('CC', 8, 3, 30716, 'Programacao Matematica', 45, 15, '990 horas', 0).
+disciplina('CC', 8, 3, 35342, 'Linguagens Formais', 45, 15, 30763, 0).
+disciplina('CC', 8, 3, 30769, 'Sistemas Distribuidos', 30, 30, 30706, 1).
+disciplina('CC', 8, 3, 35345, 'Computacao Grafica', 15, 45, 30762, 1).
+disciplina('CC', 8, 3, 30100, 'Laboratorio de Desenvolvimento', 30, 0, '990 horas', 0).
+disciplina('CC', 8, 0, 0, 'Disciplina Eletiva', 30, 0, '990 horas', 1).
+
+disciplina('CC', 9, 3, 35331, 'Simulacao', 30, 30, 30736, 1).
+disciplina('CC', 9, 3, 30717, 'Projeto de Conclusao CC', 30, 0, '1.800 horas', 0).
+disciplina('CC', 9, 3, 35359, 'Compiladores', 45, 15, 35342, 1).
+disciplina('CC', 9, 3, 30767, 'Tolerancia a Falhas – A', 30, 0, 38112, 0).
+disciplina('CC', 9, 7, 70879, 'Etica e Filosofia da Ciencia', 60, 0, 0, 1).
+disciplina('CC', 9, 3, 30462, 'Processamento de Imagens', 30, 30, 30762, 0).
+
+disciplina('CC', 10, 3, 35352, 'Trabalho de Conclusao A', 150, 10, 30904, 0).
+disciplina('CC', 10, 6, 65360, 'Empreendedor em Informatica', 60, 0, 0, 0).
+disciplina('CC', 10, 0, 0, 'Disciplina Eletiva', 30, 0, '990 horas', 1).
+
+/* SI */
+disciplina('SI', 1, 1, 10229, 'Matematica Discreta', 60, 0, 0, 1).
+disciplina('SI', 1, 3, 30461, 'Interacao Humano Computador', 30, 30, 0, 1).
+disciplina('SI', 1, 3, 30701, 'Algoritmos e Estrutura de Dados I', 30, 60, 0, 1).
+disciplina('SI', 1, 3, 30737, 'Computador e Sociedade', 30, 0, 0, 1).
+disciplina('SI', 1, 3, 35320, 'Introducao a Informatica', 45, 15, 0, 1).
+
+disciplina('SI', 2, 3, 30736 , 'Estatistica Aplicada a Computacao', 45, 15, 0, 1).
+disciplina('SI', 2, 3, 30762 , 'Linguagem de Programacao I – A', 45, 45, 30701, 0).
+disciplina('SI', 2, 7, 70879 , 'Etica e Filosofia da Ciencia', 60, 0, 0, 1).
+disciplina('SI', 2, 3, 35324 , 'Logica para a Computacao', 60, 0, 0, 1).
+disciplina('SI', 2, 3, 35327 , 'Banco de Dados I', 45, 15, 0, 1).
+
+disciplina('SI', 3, 3, 30703, 'Algoritmos e Estrutura de Dados II', 30, 30, 30701, 1).
+disciplina('SI', 3, 3, 30763, 'Fundamentos de Teoria da Computacao', 45, 15, '420h', 1).
+disciplina('SI', 3, 3, 30774, 'Sistemas Digitais', 30, 30, 0, 1).
+disciplina('SI', 3, 3, 35328, 'Banco de Dados II', 45, 15, 35327, 1).
+disciplina('SI', 3, 3, 30866, 'Empreendedorismo e Inovacao em TI', 60, 0, 0, 0).
+disciplina('SI', 3, 0, 0, 'Disciplina Eletivas', 60, 0, 0, 1).
+
+disciplina('SI', 4, 3, 30709, 'Engenharia de Software I', 60, 0, '420h', 1).
+disciplina('SI', 4, 3, 35323, 'Linguagem de Programacao II', 30, 30, 30701, 1).
+disciplina('SI', 4, 3, 38112, 'Sistemas Operacionais I', 45, 15, 0, '600h', 1).
+disciplina('SI', 4, 3, 35346, 'Inteligencia Artificial', 45, 15, 0, 1).
+disciplina('SI', 4, 3, 30764, 'Redes de Computadores I', 45, 15, '600h', 1).
+disciplina('SI', 4, 0, 0, 'Disciplina Eletiva', 60, 0, 0, 1).
+
+disciplina('SI', 5, 3, 30710, 'Engenharia de Software II', 30, 30, 30709, 1).
+disciplina('SI', 5, 3, 30765, 'Redes de Computadores II', 30, 30, 30764, 1).
+disciplina('SI', 5, 3, 35331, 'Simulacao', 30, 30, 30701, 1).
+disciplina('SI', 5, 3, 39705, 'Arquitetura de Computadores', 45, 15, 30701, 1).
+disciplina('SI', 5, 4, 40947, 'Disciplinas Eletivas', 120, 0, 0, 1).
+
+disciplina('SI', 6, 3, 30100, 'Laboratório de Desenvolvimento', 0, 30, '30762 e 35327', 0).
+disciplina('SI', 6, 3, 30711, 'Engenharia de Software III', 15, 45, 30710, 1).
+disciplina('SI', 6, 3, 30715, 'Seguranca e Auditoria de Sistemas', 30, 0, 1).
+disciplina('SI', 6, 3, 30769, 'Sistemas Distribuidos', 30, 30, 30764, 1).
+disciplina('SI', 6, 0, 0, 'Disciplinas Eletivas', 0, 0, 0, 1).
+
+disciplina('SI', 7, 3, 35345, 'Computacao Grafica', 15, 45, 30762, 1).
+disciplina('SI', 7, 3, 35359, 'Compiladores', 45, 15, 30763, 1).
+disciplina('SI', 7, 3, 30755, 'Estagio Profissional I', 0, 150, '1710h', 0).
+disciplina('SI', 7, 0, 0, 'Disciplinas Eletivas', 90, 0, 0, 1).
+
+disciplina('SI', 8, 3, 30872, 'Trabalho Final de Graduacao em SI', 60, 04, '1710h', 0).
+disciplina('SI', 8, 3, 30756, 'Estagio Profissional II', 0, 150, '1710h', 0).
+disciplina('SI', 8, 0, 0, 'Disciplinas Eletivas', 120, 0, 0, 1).
+
+
+/* 1) Listar todas as disciplinas de um determinado semestre e curso */
+disciplinasPorSemestreECurso(DISCIPLINAS, SEMESTRE, CURSO):- disciplina(CURSO, SEMESTRE, CDI, CD, DISCIPLINAS, HT, HP, PR, CCSI). /* certo */
+
+/* 2) Listar todo o curriculo de um determinado curso */
+curriculoDoCurso(DISCIPLINAS, CURSO):- disciplina(CURSO, S, CDI, CD, DISCIPLINAS, HT, HP, PR, CCSI). /* certo */
+
+/* 3) Verificar e mostrar se uma determinada disciplina faz parte de um determinado curso (CC ou SI) */
+disciplinaFazParteDoCurso(DISCIPLINA, CURSO):- disciplina(CURSO, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI). /* certo */
+
+/* 4) Verificar e mostrar se uma disciplina faz parte de mais de um curriculo (por exemplo CC e SI) */
+disciplinaCCSI(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI), CCSI=:=1.
+
+/* 5) Verificar e mostrar se uma determinada disciplina possui pré requisito*/
+temPreRequisito(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI), PR\=0. /* certo */
+
+/* 6) Listar todas as disciplinas de um curso que possuem algum tipo de pré requisito */
+disciplinasComPR(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI), PR\=0. /* certo */
+
+/* 7) Consultar as disciplinas comuns, presentes nos curriculos de CC e SI */
+disciplinaComuns(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI), CCSI=:=1.
+
+/* 8) Listar as disciplinas de um curriculo com carga horaria pratica */
+temCargaHorariaPratica(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI), HP\=0. /* certo */
+
+/* 9) Listar somente as disciplinas vinculadas a outras areas (departamentos) */
+disciplinasDeOutrasAreas(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR, CCSI), CDI\=3. /* certo */
---
+++ materiasComputacao.pl
@@ -0,0 +1,143 @@
+
+/* CC */
+disciplina('CC', 1, 3, 35320, 'Introducao a Informatica', 45, 15, 0).
+disciplina('CC', 1, 3, 30461, 'Interacao Humano Computador', 30, 30, 0).
+disciplina('CC', 1, 3, 30701, 'Algoritmos e Estrutura de Dados I', 30, 60, 0).
+disciplina('CC', 1, 3, 30737, 'Computador e Sociedade', 30, 0, 0).
+disciplina('CC', 1, 7, 70899, 'Psicologia Aplicada', 30, 0, 0).
+disciplina('CC', 1, 7, 72378, 'Metodologia da Pesquisa', 30, 0, 0).
+
+disciplina('CC', 2, 3, 30762, 'Linguagem de Programacao I A', 45, 45, 30701).
+disciplina('CC', 2, 3, 35324, 'Logica para a Computacao', 60, 0, 0).
+disciplina('CC', 2, 3, 30702, 'Fisica Aplicada a Computacao', 30, 30, 0).
+disciplina('CC', 2, 1, 10811, 'Geometria Analitica e Algebra Linear', 60, 0, 0).
+disciplina('CC', 2, 8, 81285, 'Ingles Instrumental I', 30, 0, 0).
+
+disciplina('CC', 3, 3, 30715, 'Seguranca e Auditoria de Sistemas', 30, 0, 0).
+disciplina('CC', 3, 3, 35323, 'Linguagem de Programacao II', 30, 30, 30762).
+disciplina('CC', 3, 3, 30774, 'Sistemas Digitais', 30, 30, 35324).
+disciplina('CC', 3, 3, 30703, 'Algoritmos e Estruturas de Dados II', 30, 30, 30762).
+disciplina('CC', 3, 1, 10229, 'Matematica Discreta', 60, 0, 0).
+disciplina('CC', 3, 6, 66234, 'Legislacao em Informatica', 30, 0, 0).
+
+disciplina('CC', 4, 3, 39705, 'Arquitetura de Computadores', 45, 15, 30774).
+disciplina('CC', 4, 3, 35327, 'Banco de Dados I', 45, 15, '420 horas').
+disciplina('CC', 4, 3, 30705, 'Algoritmos e Estruturas de Dados III', 30, 30, 30703).
+disciplina('CC', 4, 3, 30704, 'Linguagem de Programacao III', 30, 30, 35323).
+disciplina('CC', 4, 1, 10405, 'Calculo I', 60, 0, 0).
+
+disciplina('CC', 5, 3, 39706, 'Arquiteturas Avancadas de Computadores', 60, 0, 39705).
+disciplina('CC', 5, 3, 35328, 'Banco de Dados II', 45, 15, 35327).
+disciplina('CC', 5, 3, 30709, 'Engenharia de Software I', 45, 15, '420 horas').
+disciplina('CC', 5, 3, 38112, 'Sistemas Operacionais I', 45, 15, '420 horas').
+disciplina('CC', 5, 1, 10406, 'Calculo II', 60, 0, 10405).
+
+disciplina('CC', 6, 3, 30763, 'Fundamentos de Teoria da Computacao', 45, 15, '720 horas').
+disciplina('CC', 6, 1, 15128, 'Calculo Numerico', 45, 15, 0).
+disciplina('CC', 6, 3, 38113, 'Sistemas Operacionais II', 15, 15, 38112).
+disciplina('CC', 6, 3, 30764, 'Redes de Computadores I', 45, 15, '720 horas').
+disciplina('CC', 6, 3, 30706, 'Linguagem de Programacao IV', 15, 15, 30762).
+disciplina('CC', 6, 3, 30710, 'Engenharia de Software II', 30, 30, 30709).
+
+disciplina('CC', 7, 3, 30736, 'Estatistica Aplicada a Computacao', 45, 15, 0).
+disciplina('CC', 7, 0, 0, 'Disciplina Eletiva', 60, 0, '990 horas').
+disciplina('CC', 7, 3, 35346, 'Inteligencia Artificial', 45, 15, 35324).
+disciplina('CC', 7, 3, 30765, 'Redes de Computadores II', 30, 30, 30764).
+disciplina('CC', 7, 3, 30711, 'Engenharia de Software III', 15, 45, 30710).
+
+disciplina('CC', 8, 3, 30716, 'Programacao Matematica', 45, 15, '990 horas').
+disciplina('CC', 8, 3, 35342, 'Linguagens Formais', 45, 15, 30763).
+disciplina('CC', 8, 3, 30769, 'Sistemas Distribuidos', 30, 30, 30706).
+disciplina('CC', 8, 3, 35345, 'Computacao Grafica', 15, 45, 30762).
+disciplina('CC', 8, 3, 30100, 'Laboratorio de Desenvolvimento', 30, 0, '990 horas').
+disciplina('CC', 8, 0, 0, 'Disciplina Eletiva', 30, 0, '990 horas').
+
+disciplina('CC', 9, 3, 35331, 'Simulacao', 30, 30, 30736).
+disciplina('CC', 9, 3, 30717, 'Projeto de Conclusao CC', 30, 0, '1.800 horas').
+disciplina('CC', 9, 3, 35359, 'Compiladores', 45, 15, 35342).
+disciplina('CC', 9, 3, 30767, 'Tolerancia a Falhas – A', 30, 0, 38112).
+disciplina('CC', 9, 7, 70879, 'Etica e Filosofia da Ciencia', 60, 0, 0).
+disciplina('CC', 9, 3, 30462, 'Processamento de Imagens', 30, 30, 30762).
+
+disciplina('CC', 10, 3, 35352, 'Trabalho de Conclusao A', 150, 10, 30904).
+disciplina('CC', 10, 6, 65360, 'Empreendedor em Informatica', 60, 0, 0).
+disciplina('CC', 10, 0, 0, 'Disciplina Eletiva', 30, 0, '990 horas').
+
+/* SI */
+disciplina('SI', 1, 1, 10229, 'Matematica Discreta', 60, 0, 0).
+disciplina('SI', 1, 3, 30461, 'Interacao Humano Computador', 30, 30, 0).
+disciplina('SI', 1, 3, 30701, 'Algoritmos e Estrutura de Dados I', 30, 60, 0).
+disciplina('SI', 1, 3, 30737, 'Computador e Sociedade', 30, 0, 0).
+disciplina('SI', 1, 3, 35320, 'Introducao a Informatica', 45, 15, 0).
+
+disciplina('SI', 2, 3, 30736 , 'Estatistica Aplicada a Computacao', 45, 15, 0).
+disciplina('SI', 2, 3, 30762 , 'Linguagem de Programacao I – A', 45, 45, 30701).
+disciplina('SI', 2, 7, 70879 , 'Etica e Filosofia da Ciencia', 60, 0, 0).
+disciplina('SI', 2, 3, 35324 , 'Logica para a Computacao', 60, 0, 0).
+disciplina('SI', 2, 3, 35327 , 'Banco de Dados I', 45, 15, 0).
+
+disciplina('SI', 3, 3, 30703, 'Algoritmos e Estrutura de Dados II', 30, 30, 30701).
+disciplina('SI', 3, 3, 30763, 'Fundamentos de Teoria da Computacao', 45, 15, '420h').
+disciplina('SI', 3, 3, 30774, 'Sistemas Digitais', 30, 30, 0).
+disciplina('SI', 3, 3, 35328, 'Banco de Dados II', 45, 15, 35327).
+disciplina('SI', 3, 3, 30866, 'Empreendedorismo e Inovacao em TI', 60, 0, 0).
+disciplina('SI', 3, 0, 0, 'Disciplina Eletivas', 60, 0, 0).
+
+disciplina('SI', 4, 3, 30709, 'Engenharia de Software I', 60, 0, '420h').
+disciplina('SI', 4, 3, 35323, 'Linguagem de Programacao II', 30, 30, 30701).
+disciplina('SI', 4, 3, 38112, 'Sistemas Operacionais I', 45, 15, 0, '600h').
+disciplina('SI', 4, 3, 35346, 'Inteligencia Artificial', 45, 15, 0).
+disciplina('SI', 4, 3, 30764, 'Redes de Computadores I', 45, 15, '600h').
+disciplina('SI', 4, 0, 0, 'Disciplina Eletiva', 60, 0, 0).
+
+disciplina('SI', 5, 3, 30710, 'Engenharia de Software II', 30, 30, 30709).
+disciplina('SI', 5, 3, 30765, 'Redes de Computadores II', 30, 30, 30764).
+disciplina('SI', 5, 3, 35331, 'Simulacao', 30, 30, 30701).
+disciplina('SI', 5, 3, 39705, 'Arquitetura de Computadores', 45, 15, 30701).
+disciplina('SI', 5, 4, 40947, 'Disciplinas Eletivas', 120, 0, 0).
+
+disciplina('SI', 6, 3, 30100, 'Laboratório de Desenvolvimento', 0, 30, '30762 e 35327').
+disciplina('SI', 6, 3, 30711, 'Engenharia de Software III', 15, 45, 30710).
+disciplina('SI', 6, 3, 30715, 'Seguranca e Auditoria de Sistemas', 30, 0).
+disciplina('SI', 6, 3, 30769, 'Sistemas Distribuidos', 30, 30, 30764).
+disciplina('SI', 6, 0, 0, 'Disciplinas Eletivas', 0, 0, 0).
+
+disciplina('SI', 7, 3, 35345, 'Computacao Grafica', 15, 45, 30762).
+disciplina('SI', 7, 3, 35359, 'Compiladores', 45, 15, 30763).
+disciplina('SI', 7, 3, 30755, 'Estagio Profissional I', 0, 150, '1710h').
+disciplina('SI', 7, 0, 0, 'Disciplinas Eletivas', 90, 0, 0).
+
+disciplina('SI', 8, 3, 30872, 'Trabalho Final de Graduacao em SI', 60, 04, '1710h').
+disciplina('SI', 8, 3, 30756, 'Estagio Profissional II', 0, 150, '1710h').
+disciplina('SI', 8, 0, 0, 'Disciplinas Eletivas', 120, 0, 0).
+
+disciplinaComum(CABECA, ['Introducao a Informatica', 'Interacao Humano Computador', 'Algoritmos e Estrutura de Dados I', 'Computador e Sociedade', 'Logica para a Computacao', 'Seguranca e Auditoria de Sistemas', 'Linguagem de Programacao II', 'Sistemas Digitais', 'Matematica Discreta', 'Arquitetura de Computadores', 'Banco de Dados I', 'Banco de Dados II', 'Engenharia de Software I', 'Sistemas Operacionais I', 'Fundamentos de Teoria da Computacao', 'Redes de Computadores I', 'Engenharia de Software II', 'Estatistica Aplicada a Computacao', 'Disciplina Eletiva', 'Inteligencia Artificial', 'Redes de Computadores II', 'Engenharia de Software III', 'Sistemas Distribuidos', 'Computacao Grafica', 'Simulacao', 'Compiladores', 'Etica e Filosofia da Ciencia']).
+
+
+
+/* 1) Listar todas as disciplinas de um determinado semestre e curso */
+disciplinasPorSemestreECurso(DISCIPLINAS, SEMESTRE, CURSO):- disciplina(CURSO, SEMESTRE, CDI, CD, DISCIPLINAS, HT, HP, PR). /* certo */
+
+/* 2) Listar todo o curriculo de um determinado curso */
+curriculoDoCurso(DISCIPLINAS, CURSO):- disciplina(CURSO, S, CDI, CD, DISCIPLINAS, HT, HP, PR). /* certo */
+
+/* 3) Verificar e mostrar se uma determinada disciplina faz parte de um determinado curso (CC ou SI) */
+disciplinaFazParteDoCurso(DISCIPLINA, CURSO):- disciplina(CURSO, S, CDI, CD, DISCIPLINA, HT, HP, PR). /* certo */
+
+/* 4) Verificar e mostrar se uma disciplina faz parte de mais de um curriculo (por exemplo CC e SI) */
+disciplinaCCSI(DISCIPLINA, M1, M2):- disciplina(M1, S, CDI, CD, DISCIPLINA, HT, HP, PR), disciplina(M2, S, CDI, CD, DISCIPLINA, HT, HP, PR).
+
+/* 5) Verificar e mostrar se uma determinada disciplina possui pré requisito*/
+temPreRequisito(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR), PR\=0. /* certo */
+
+/* 6) Listar todas as disciplinas de um curso que possuem algum tipo de pré requisito */
+disciplinasComPR(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR), PR\=0. /* certo */
+
+/* 7) Consultar as disciplinas comuns, presentes nos curriculos de CC e SI */
+disciplinaComuns(DISCIPLINA):- disciplina('CC', S, CDI, CD, DISCIPLINA, HT, HP, PR), disciplina('SI', S, CDI, CD, DISCIPLINA, HT, HP, PR).
+
+/* 8) Listar as disciplinas de um curriculo com carga horaria pratica */
+temCargaHorariaPratica(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR), HP\=0. /* certo */
+
+/* 9) Listar somente as disciplinas vinculadas a outras areas (departamentos) */
+disciplinasDeOutrasAreas(DISCIPLINA):- disciplina(C, S, CDI, CD, DISCIPLINA, HT, HP, PR), CDI\=3. /* certo */
